93de07a536ed764ab9c01bc877b854fbabfb3783
Omar Hussein <omar.hussein337@gmail.com>
2020-05-29 13:33:25 +0200

    Updates

---
+++ precache-manifest.c6121c2a576457c59ece16b894d0b563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bad63cedda592d2b9f34abee9748125",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "0c806e0f1f8e9c57c95d",
+    "url": "/to-do-app/static/css/main.64584ac1.chunk.css"
+  },
+  {
+    "revision": "fd7e664352214c515314",
+    "url": "/to-do-app/static/js/2.14973a85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.14973a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c806e0f1f8e9c57c95d",
+    "url": "/to-do-app/static/js/main.46971480.chunk.js"
+  },
+  {
+    "revision": "66a73fc8e0bf0e04d24a",
+    "url": "/to-do-app/static/js/runtime-main.1267b5ee.js"
+  }
+]);
